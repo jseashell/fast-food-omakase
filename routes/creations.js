c48d9c0becd8b1c046944b7986fba4fee6fc7a73
John Schellinger <jschellinger@orchardsoft.com>
2020-09-24 11:59:03 -0400

    Add navigation to/from Home and Creations pages

--- routes/creations.js
+++ routes/creations.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const creationsTitle = 'Creations';
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-	res.render('creations', { title: 'Creations' });
+	res.render('creations',
+		{
+			title: creationsTitle,
+			creationsTitle: creationsTitle
+		});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Fast Food Omakase' });
+	res.render('index',
+		{
+			title: 'Fast Food Omakase',
+			creationsTitle: 'Creations'
+		});
 });
 
 module.exports = router;
