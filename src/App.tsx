77ae92cfbe576ddf160eb8ff2c8b6f947db7b8db
John Schellinger <schellingerj1@gmail.com>
2020-09-26 20:56:20 -0400

    Add Creation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from 'react';
-
-interface CreationUI {
-  id: string;
-  name: string;
-  image: string;
-  user: string;
-}
+import Creation from './components/Creation';
 
 function App() {
   // Prepare state hook for welcome message
   const [welcomeMessage, setWelcomeMessage] = useState('')
 
   // Prepare state hook for users list
-  // Note: <CreationUI[]> is for TypeScript
-  // It specifies the shape of creationsList state
-  const [creationsList, setCreationsList] = useState<CreationUI[]>([])
+  // Note: <Creation[]> is for TypeScript. It specifies the shape of creationsList state
+  const [creationsList, setCreationsList] = useState<Creation[]>([])
 
   const fetchMessage = async () => {
     const message = await fetch('/api')
@@ -41,27 +34,11 @@ function App() {
       <header className="app-header">
         <p>{welcomeMessage}</p>
         <button onClick={fetchCreations}>Fetch creations</button>
-        {creationsList.length > 0 && <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Image</th>
-              <th>User</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {creationsList.map((creation: CreationUI) => (
-              <tr key={creation.id}>
-                <td>{creation.id}</td>
-                <td>{creation.name}</td>
-                <td>{creation.image}</td>
-                <td>{creation.user}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>}
+        {creationsList.length > 0 && <div className="creations-container">
+          {creationsList.map((creation: Creation) => (
+            <Creation /> // How do I map an entry in `creationsList` array to a `./components/Creation` component?
+          ))}
+        </div>}
       </header>
     </div>
   )
